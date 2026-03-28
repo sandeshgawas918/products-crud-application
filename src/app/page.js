7abaae49e7bb14979e382ca5042e406d9eaf1e0e
sandesh gawas <sandeshgawas918@gmail.com>
2026-03-28 14:53:00 +0530

    mobile friendly ui

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,8 @@ const DisplayProducts = lazy(()=>import('@/app/components/DisplayProducts'))
 
 export default function Home() {
   return (
-    <div className="flex flex-col mt-10 mb-20 items-center justify-center font-sans dark:bg-black">
-      <h1 className="text-2xl font-semibold italic underline underline-offset-4">Products CRUD Application</h1>
+    <div className="flex flex-col mt-10 mb-20 md:items-center md:justify-center font-sans dark:bg-black">
+      <h1 className="text-2xl text-center font-semibold italic underline underline-offset-4">Products CRUD Application</h1>
       <Suspense fallback={<p className="text-2xl text-gray-400">Loading...</p>}>
         <DisplayProducts />
       </Suspense>
